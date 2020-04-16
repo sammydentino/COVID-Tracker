295b7c93481939ca16b66f468ec34ed080d9f44d
BuildTools <unconfigured@null.spigotmc.org>
2020-04-15 21:41:20 -0700

    Initial Commit

---
+++ COVID-19 Data/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  COVID-19 Data
+//
+//  Created by Sammy Dentino on 4/15/20.
+//  Copyright © 2020 Sammy Dentino. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
