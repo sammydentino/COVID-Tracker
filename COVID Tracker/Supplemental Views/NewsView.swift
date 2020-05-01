bc68e05f12169a24d0012b29ac45e93aa4b85d2f
sammydentino <sammydentino@outlook.com>
2020-05-01 16:20:08 -0500

    Switched to a News API that doesn't cause crash when under multiple API calls but at the expense of the items not being updated as frequently, but seems to be the best option at the moment

--- COVID Tracker/Supplemental Views/NewsView.swift
+++ COVID Tracker/Supplemental Views/NewsView.swift
@@ -17,7 +17,7 @@ struct NewsView: View {
 	
 	var body: some View {
 		VStack(alignment: .leading) {
-			List (fetch.news.articles) { item in
+			List (fetch.news) { item in
 				NavigationLink(destination: WebView(request: URLRequest(url: URL(string: item.url)!)).navigationBarTitle(Text(item.title), displayMode: .inline)) {
 					VStack (alignment: .leading){
 						HStack {
@@ -28,7 +28,7 @@ struct NewsView: View {
 									.clipped()
 									.clipShape(Circle())
 							}
-							.frame(width: 60, height: 60)
+							.frame(width: 55, height: 55)
 							.padding(EdgeInsets(top: 7.5, leading: 7.5, bottom: 0, trailing: 0))
 							Text(item.title)
 								.font(.subheadline)
@@ -37,40 +37,13 @@ struct NewsView: View {
 								.padding(EdgeInsets(top: 7.5, leading: 7.5, bottom: 0, trailing: 0))
 						}
 						Spacer()
-						Text(item.source.name)
+						Text(item.publication)
 							.font(.subheadline)
 							.foregroundColor(.gray)
 							.bold()
 							.padding(EdgeInsets(top: 5, leading: 7.5, bottom: 7.5, trailing: 0))
 					}
 				}
-				/*Button(action: {
-					self.showingDetail.toggle()
-				}) {
-					VStack (alignment: .leading){
-						HStack {
-							URLImage(URLRequest(url: URL(string: item.urlToImage)!), delay: 0.25) { proxy in
-								proxy.image
-									.resizable()
-									.aspectRatio(contentMode: .fill)
-									.clipped()
-									.clipShape(Circle())
-							}
-							.frame(width: 60, height: 60)
-							.padding(EdgeInsets(top: 7.5, leading: 7.5, bottom: 0, trailing: 0))
-							Text(item.title)
-								.font(.subheadline)
-								.bold()
-								.lineLimit(3)
-								.padding(EdgeInsets(top: 7.5, leading: 7.5, bottom: 0, trailing: 0))
-						}
-						Spacer()
-					}
-				}.sheet(isPresented: self.$showingDetail) {
-					NavigationView {
-						 WebView(request: URLRequest(url: URL(string: item.url)!)).navigationBarTitle(Text(item.title), displayMode: .inline)
-					}
-				}*/
 			}
 		}
 	}
@@ -95,19 +68,19 @@ struct WebView : UIViewRepresentable {
 }
 
 class getNews : ObservableObject {
-	@Published var news : News!
+	@Published var news : [News]!
 	
 	init() {
 		loadNews()
 	}
 	func loadNews() {
-		let urlString = "https://newsapi.org/v2/everything?q=COVID-19&sortBy=publishedAt&language=en&apiKey=d892aab794d64e4899eb9f61129167ed&pageSize=100&page=1"
-		
+		let urlString = "https://covidtracking.com/api/press"
+
 		if let url = URL(string: urlString) {
 			if let d = try? Data(contentsOf: url) {
 				// we're OK to parse!
 				let decoder = JSONDecoder()
-				if let data = try? decoder.decode(News.self, from: d) {
+				if let data = try? decoder.decode([News].self, from: d) {
 					news = data
 				}
 			}
@@ -115,72 +88,27 @@ class getNews : ObservableObject {
 	}
 }
 
-struct News : Codable {
-	let status : String!
-	let totalResults : Int!
-	let articles : [Articles]!
-
-	enum CodingKeys: String, CodingKey {
-		case status = "status"
-		case totalResults = "totalResults"
-		case articles = "articles"
-	}
-
-	init(from decoder: Decoder) throws {
-		let values = try decoder.container(keyedBy: CodingKeys.self)
-		status = try values.decodeIfPresent(String.self, forKey: .status)
-		totalResults = try values.decodeIfPresent(Int.self, forKey: .totalResults)
-		articles = try values.decodeIfPresent([Articles].self, forKey: .articles)
-	}
-}
-
-struct Articles : Codable, Identifiable {
+struct News : Codable, Identifiable {
 	let id = UUID()
-	let source : Source!
-	let author : String!
 	let title : String!
-	let description : String!
 	let url : String!
-	let urlToImage : String!
-	let publishedAt : String?
-	let content : String?
+	let publication : String!
+	var urlToImage: String!
+	let publishDate : String!
 
 	enum CodingKeys: String, CodingKey {
-		case source = "source"
-		case author = "author"
 		case title = "title"
-		case description = "description"
 		case url = "url"
-		case urlToImage = "urlToImage"
-		case publishedAt = "publishedAt"
-		case content = "content"
+		case publication = "publication"
+		case publishDate = "publishDate"
 	}
 
 	init(from decoder: Decoder) throws {
 		let values = try decoder.container(keyedBy: CodingKeys.self)
-		source = try values.decodeIfPresent(Source.self, forKey: .source)
-		author = try values.decodeIfPresent(String.self, forKey: .author) ?? "N/A"
-		title = try values.decodeIfPresent(String.self, forKey: .title) ?? "N/A"
-		description = try values.decodeIfPresent(String.self, forKey: .description) ?? "N/A"
-		url = try values.decodeIfPresent(String.self, forKey: .url) ?? "https://www.google.com/"
-		urlToImage = try values.decodeIfPresent(String.self, forKey: .urlToImage) ?? "https://www.google.com/"
-		publishedAt = try values.decodeIfPresent(String.self, forKey: .publishedAt)
-		content = try values.decodeIfPresent(String.self, forKey: .content)
-	}
-}
-
-struct Source : Codable {
-	let id : String?
-	let name : String!
-
-	enum CodingKeys: String, CodingKey {
-		case id = "id"
-		case name = "name"
-	}
-
-	init(from decoder: Decoder) throws {
-		let values = try decoder.container(keyedBy: CodingKeys.self)
-		id = try values.decodeIfPresent(String.self, forKey: .id)
-		name = try values.decodeIfPresent(String.self, forKey: .name) ?? "N/A"
+		title = try values.decodeIfPresent(String.self, forKey: .title) ?? "Not Available"
+		url = try values.decodeIfPresent(String.self, forKey: .url) ?? "www.google.com"
+		publication = try values.decodeIfPresent(String.self, forKey: .publication) ?? "N/A"
+		publishDate = try values.decodeIfPresent(String.self, forKey: .publishDate) ?? "N/A"
+		urlToImage = "https://www.google.com/s2/favicons?domain_url=" + url
 	}
 }
