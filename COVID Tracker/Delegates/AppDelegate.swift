0a50772a9c4c9a77d19df80bc08b0ac3cb8f14ba
Sammy Dentino <sammydentino@outlook.com>
2020-06-05 17:40:38 -0500

    Version bump

--- COVID Tracker/Delegates/AppDelegate.swift
+++ COVID Tracker/Delegates/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import GoogleMobileAds
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -14,6 +15,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 	func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
 		// Override point for customization after application launch.
+        GADMobileAds.sharedInstance().start(completionHandler: nil)
 		return true
 	}
 
