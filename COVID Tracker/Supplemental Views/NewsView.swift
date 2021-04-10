0a7eae1303d472b75e2cc26d537af5a7d3d9cfab
Sammy Dentino <sammydentino@outlook.com>
2021-04-10 16:37:53 -0700

    ui fixes

--- COVID Tracker/Supplemental Views/NewsView.swift
+++ COVID Tracker/Supplemental Views/NewsView.swift
@@ -18,49 +18,51 @@ struct NewsView: View {
 	
 	var body: some View {
         List {
-            ForEach(fetch.news, id: \.id) { item in
-                Button(action: {
-                    presentingSafariView = true
-                }) {
-                    VStack (alignment: .leading){
-                        HStack {
-                            URLImage(URLRequest(url: URL(string: item.image)!), delay: 0.25) { proxy in
-                                proxy.image
-                                    .resizable()
-                                    .aspectRatio(contentMode: .fill)
-                                    .clipped()
-                                    .clipShape(Circle())
+            Section(header: Text("\n").padding(.top, -30), footer: Text("\n\n\n")) {
+                ForEach(fetch.news, id: \.id) { item in
+                    Button(action: {
+                        presentingSafariView = true
+                    }) {
+                        VStack (alignment: .leading){
+                            HStack {
+                                URLImage(URLRequest(url: URL(string: item.image)!), delay: 0.25) { proxy in
+                                    proxy.image
+                                        .resizable()
+                                        .aspectRatio(contentMode: .fill)
+                                        .clipped()
+                                        .clipShape(Circle())
+                                }
+                                .frame(width: 55, height: 55)
+                                .padding(EdgeInsets(top: 7.5, leading: 0, bottom: 0, trailing: 5))
+                                Text(item.title)
+                                    .font(.subheadline)
+                                    .bold()
+                                    .lineLimit(3)
+                                    .foregroundColor(.primary)
+                                    .padding(EdgeInsets(top: 7.5, leading: 0, bottom: 0, trailing: 0))
                             }
-                            .frame(width: 55, height: 55)
-                            .padding(EdgeInsets(top: 7.5, leading: 0, bottom: 0, trailing: 5))
-                            Text(item.title)
+                            Spacer()
+                            Text(item.description)
                                 .font(.subheadline)
+                                .foregroundColor(.gray)
                                 .bold()
                                 .lineLimit(3)
-                                .foregroundColor(.primary)
-                                .padding(EdgeInsets(top: 7.5, leading: 0, bottom: 0, trailing: 0))
+                                .padding(EdgeInsets(top: 5, leading: 0, bottom: 7.5, trailing: 0))
                         }
-                        Spacer()
-                        Text(item.description)
-                            .font(.subheadline)
-                            .foregroundColor(.gray)
-                            .bold()
-                            .lineLimit(3)
-                            .padding(EdgeInsets(top: 5, leading: 0, bottom: 7.5, trailing: 0))
-                    }
-                }.sheet(isPresented: $presentingSafariView) {
-                    SafariView(
-                        url: URL(string: item.url!)!,
-                        configuration: SafariView.Configuration(
-                            entersReaderIfAvailable: true,
-                            barCollapsingEnabled: false
+                    }.sheet(isPresented: $presentingSafariView) {
+                        SafariView(
+                            url: URL(string: item.url!)!,
+                            configuration: SafariView.Configuration(
+                                entersReaderIfAvailable: true,
+                                barCollapsingEnabled: false
+                            )
                         )
-                    )
-                    .preferredBarAccentColor(.white)
-                    .preferredControlAccentColor(.accentColor)
-                    .dismissButtonStyle(.done)
+                        .preferredBarAccentColor(.white)
+                        .preferredControlAccentColor(.accentColor)
+                        .dismissButtonStyle(.done)
+                    }
                 }
             }
-        }.listStyle(PlainListStyle())
+        }.fixList()
 	}
 }
